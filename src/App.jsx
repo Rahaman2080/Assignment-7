0507773e49debdb0cd2683e4428170bb5895ff53
Rahaman2080 <rahaman2080@gmail.com>
2023-09-16 02:08:37 +0600

    Total time and remaining time added with validitation

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,29 @@ import { useState } from 'react';
 
 function App() {
   const [cardTitle, setCardTitle] = useState([]);
- 
-
+  const [residual, setResidual] = useState(0);
+  const [totalHour, setTotalHour] = useState(0);
+  
 
   const handleSelectBtn = card => {
     const isAvailable = cardTitle.find(item=>item.id == card.id);
 
-    
+    let totalTime = card.credit_time;
     if(isAvailable){
       alert("Already selected");
     }else{
-      setCardTitle([...cardTitle, card]);
+      cardTitle.forEach((item) => {
+        totalTime = totalTime + item.credit_time;
+      }); 
+
+      const totalRemaining = 20 - totalTime;
+      if(totalTime > 20){
+        return alert('Maximum time is not allowed')
+      } else{
+        setResidual(totalRemaining);
+        setTotalHour(totalTime);
+        setCardTitle([...cardTitle, card]);
+      }
       
     }  
   };
@@ -29,7 +41,8 @@ function App() {
        <Cards handleSelectBtn ={handleSelectBtn}></Cards>
        <Carts 
        cardTitle={cardTitle}
-      
+       residual={residual}
+       totalHour={totalHour}
        ></Carts>
        </div>
       </div>
--- src/Components/Cart/Carts.jsx
+++ src/Components/Cart/Carts.jsx
@@ -2,18 +2,18 @@
 import PropTypes from 'prop-types';
 import Cart from './Cart';
 
-const Carts = ({cardTitle}) => {
+const Carts = ({cardTitle, residual, totalHour}) => {
     return (
         <div>
             <div className="w-72 h-auto bg-white rounded-md m-4 p-4">
-                <h3 className="text-xl font-bold text-blue-700 py-3">Hour remaining {}hr</h3>
+                <h3 className="text-xl font-bold text-blue-700 py-3">Hour remaining {residual}hr</h3>
                 <hr className='h-0.5 bg-gray-400'></hr>
                 <h3 className="text-xl font-bold py-3">Course name:</h3>
                 {
                     cardTitle.map( cart => <Cart key={cart.id} cart={cart}></Cart> )
                 }
                 <hr className='h-0.5 bg-gray-400'></hr>
-                <h3 className='text-xl font-bold  py-3'>Total credit hour: {}</h3>
+                <h3 className='text-xl font-bold  py-3'>Total credit hour: {totalHour}</h3>
                 <hr className='h-0.5 bg-gray-400'></hr>
                 <h3 className='text-xl font-bold py-3'>Total price: {} USD</h3>
 
