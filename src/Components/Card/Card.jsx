bdf831aa921b4c77432e484805d53cbc4b655d0e
Rahaman2080 <rahaman2080@gmail.com>
2023-09-16 16:19:33 +0600

    updated readme file and modified json file

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -6,11 +6,11 @@ import {BsBook} from 'react-icons/bs'
 const Card = ({card, handleSelectBtn}) => {
     const {img, title, details, price, credit_time} = card;
     return (
-        <div className="w-72 h-96 rounded-md mx-2 p-3 bg-white text-center m-2">
+        <div className="w-72 h-auto rounded-md mx-2 p-3 bg-white text-center m-2">
                 <div className="space-y-4">
                     <img className="w-full rounded-md" src={img} alt="" />
                     <h4 className="font-bold text-lg">{title}</h4>
-                    <p className="text-sm text-gray-600">{details}</p>
+                    <p className="text-sm text-gray-600 h-24">{details}</p>
                     <div className="flex justify-between items-center">
                      <p className='flex items-center gap-2'><span><FiDollarSign></FiDollarSign> </span>Price: {price}</p>
                     <p className='flex items-center gap-2'><span><BsBook></BsBook></span>Credit: {credit_time}hr</p>
